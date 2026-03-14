bba24e8d06768685bf7502b79ccfe0d264a94b81
xuxin1955 <73999603+xuxin1955@users.noreply.github.com>
2026-03-15 01:26:05 +0800

    Update diy-part4.sh

--- diy-part4.sh
+++ diy-part4.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-cd openwrt || { echo "openwrt 目录不存在"; exit 1; }
-rm -f target/linux/generic/backport-6.12/791-v7.0-net-phy-realtek-add-rtl822x-driver.patch
-rm -f target/linux/generic/backport-6.12/792-v7.0-net-phy-realtek-add-rtl8125b-support.patch
-rm -f target/linux/generic/backport-6.12/793-v7.0-net-phy-realtek-fix-in-band-capabilities-for-2.5G-PH.patch
-rm -f target/linux/generic/backport-6.12/794-v7.0-net-phy-realtek-fix-led-blinking.patch
+
 # turboacc
 # curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh --no-sfe
 curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
