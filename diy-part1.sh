b1289cf55b07d4c173e0dadb7456f3a0a92d3989
gitpwz <pwzpwz@gmail.com>
2026-03-19 12:09:09 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -22,13 +22,13 @@
 # echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 
 # hlk7628n dts
-mkdir -p target/linux/ramips/dts/
-cp -f "$GITHUB_WORKSPACE/dts/mt7628an_hilink_hlk-7628n.dts" target/linux/ramips/dts/mt7628an_hilink_hlk-7628n.dts
+#mkdir -p target/linux/ramips/dts/
+#cp -f "$GITHUB_WORKSPACE/dts/mt7628an_hilink_hlk-7628n.dts" target/linux/ramips/dts/mt7628an_hilink_hlk-7628n.dts
 
 
 # turboacc
 # curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
-curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh --no-sfe
+#curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh --no-sfe
 
 # OpenClash
 # git clone --depth 1 https://github.com/vernesong/OpenClash.git OpenClash
