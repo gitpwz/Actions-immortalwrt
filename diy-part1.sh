2284e2e20f91181994221d8b47debc167396f6ad
gitpwz <pwzpwz@gmail.com>
2026-03-25 22:43:31 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -37,4 +37,5 @@
 # istore
 echo >> feeds.conf.default
 echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
-
+./scripts/feeds update istore
+./scripts/feeds install -d y -p istore luci-app-store
